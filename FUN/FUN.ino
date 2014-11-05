5c471552a39920debed3771c3d6feb5db8996ad5
Angel Martinez <angelm2012@gmail.com>
2014-11-05 15:36:05 -0600

    Added FUN
    Testing Button inputs

---
+++ FUN/FUN.ino
@@ -0,0 +1,36 @@
+#include <ColorLCDShield.h>
+
+LCDShield lcd;
+
+int buttonPins[3] = {3, 4, 5};
+
+void setup()
+{
+    lcd.init(EPSON);
+    lcd.contrast(-30);
+    lcd.clear(WHITE);
+    
+    for(int i = 0; i < 3; i++)
+    {
+        pinMode(buttonPins[i], INPUT);
+        digitalWrite(buttonPins[i], HIGH);
+    }
+}
+
+void loop()
+{
+  VAMP();
+}
+
+void VAMP()// Removing the not from digitalRead if's will create a pretty cool wipe screen between the two colors
+{
+    if(!digitalRead(buttonPins[0]))
+    {
+      lcd.clear(EMERALD);
+    }
+      
+     if(!digitalRead(buttonPins[2]))
+     {
+       lcd.clear(BLACK);
+     }
+}
