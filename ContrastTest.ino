eaccc5347d910d9ea481a60fe128a8ae3b9e3052
Angel Martinez <angelm2012@gmail.com>
2014-11-05 12:59:45 -0600

    Test for optimal contrast

---
+++ ContrastTest.ino
@@ -0,0 +1,28 @@
+#include <ColorLCDShield.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define BACKGROUND  BLACK  // room for growth, adjust the background color according to daylight
+
+LCDShield lcd;
+
+void setup()
+{  
+  /* Initialize the LCD, set the contrast, clear the screen */
+  lcd.init(EPSON);
+  for(int i =0; i <60 ; i++)
+  {
+     lcd.contrast(-40 + i);
+     lcd.setChar('c', 50, 50, WHITE, BACKGROUND);
+     delay(500);
+     
+  }
+  lcd.clear(GREEN);
+  
+}
+
+void loop()
+{
+}
+
