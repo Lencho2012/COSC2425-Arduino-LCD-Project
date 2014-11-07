ae1d4e5fab1cd0835d8946a08e686f39680f256c
Angel Martinez <angelm2012@gmail.com>
2014-11-07 16:11:05 -0600

    Update Blob.ino

--- FUN/Blob.ino
+++ FUN/Blob.ino
@@ -3,7 +3,7 @@
 #include <time.h>
 
 LCDSheild lcd;
-
+//int num = 0;
 void setup()
 {
     lcd.init(EPSON);
@@ -18,6 +18,13 @@ void loop()
 
 void bolts()
 {
+/*
+	srand(time(NULL));
+	
+	num = rand() % 106; // Add 4 when setRect to avoid first 2 pixels on y axis
+	printf("%d\n", num);
+*/
+
     srand(time(NULL));
     lcd.setRect(10, 50, 30, 70, 1, BLACK);
     
